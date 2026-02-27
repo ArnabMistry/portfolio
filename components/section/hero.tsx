7e318eea08626da3a8af302899c4f7608ed26302
Arnab Mistry <88276146+ArnabMistry@users.noreply.github.com>
2026-02-28 03:12:27 +0530

    Refactor the hero section text

---
+++ components/section/hero.tsx
@@ -0,0 +1,24 @@
+// this section jwill just return the text for the hero section.
+"use client";
+
+export default function Hero() {
+  return (
+    <section className="pt-28 flex items-center">
+      <div className="w-full px-10 md:px-20">
+        <h1 className="text-white text-7xl md:text-[10vw] font-chunk leading-none">
+          PRECISION
+        </h1>
+        <div className="flex justify-center my-6">
+          <h2 className="text-7xl md:text-[10vw] font-alexbrush text-blue-500">
+            and
+          </h2>
+        </div>
+        <div className="flex justify-end">
+          <h1 className="text-white text-7xl md:text-[10vw] font-chunk leading-none">
+            DEDICATION
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+}
