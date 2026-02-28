790a4d30554ca447526fd1d54e79c2b96550c3d8
Arnab Mistry <88276146+ArnabMistry@users.noreply.github.com>
2026-02-28 11:45:08 +0530

    Refactor Hero Heading

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,6 @@ import Hero from "@/components/section/hero";
 export default function Home() {
   return (
     <main>
-      <div className="px-20 pt-11 flex items-center justify-between">
-        <h1 className="text-white text-sm font-semibold tracking-widest font-brolimo">
-          ARNAB MISTRY
-        </h1>
-        <h1 className="text-white text-sm font-semibold tracking-widest font-brolimo">
-          SUIII
-        </h1>
-      </div>
       <Hero />
     </main>
   );
--- components/section/hero.tsx
+++ components/section/hero.tsx
@@ -3,22 +3,32 @@
 
 export default function Hero() {
   return (
-    <section className="pt-12 flex items-center">
-      <div className="w-full px-10 md:px-20">
-        <h1 className="text-white text-7xl md:text-[10vw] font-chunk leading-none">
-          PRECISION
+    <>
+      <div className="px-20 pt-11 flex items-center justify-between">
+        <h1 className="text-white text-sm font-semibold tracking-widest font-brolimo">
+          ARNAB MISTRY
         </h1>
-        <div className="flex justify-center my-6">
-          <h2 className="text-7xl md:text-[12vw] font-alexbrush text-blue-500">
-            and
-          </h2>
-        </div>
-        <div className="flex justify-end">
-          <h1 className="text-white text-7xl md:text-[10vw] font-chunk leading-none">
-            DEDICATION
-          </h1>
-        </div>
+        <h1 className="text-white text-sm font-semibold tracking-widest font-brolimo">
+          SUIII
+        </h1>
       </div>
-    </section>
+      <section className="pt-12 flex items-center">
+        <div className="w-full px-10 md:px-20">
+          <h1 className="text-white text-7xl md:text-[10vw] font-chunk leading-none">
+            PRECISION
+          </h1>
+          <div className="flex justify-center my-6">
+            <h2 className="text-7xl md:text-[12vw] font-alexbrush text-blue-500">
+              and
+            </h2>
+          </div>
+          <div className="flex justify-end">
+            <h1 className="text-white text-7xl md:text-[10vw] font-chunk leading-none">
+              DEDICATION
+            </h1>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
